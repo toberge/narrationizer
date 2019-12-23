4ae6c5419869b480e4455bbe6e3c1d72dc6a38e0
toberge <toberge@stud.ntnu.no>
2019-12-23 19:35:08 +0100

    Updates

---
+++ precache-manifest.a030473ef7e5a2db1d6cc9c6784cf3c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75322b495b1f8439c6d1a0210ffc3f9",
+    "url": "/narrationizer/index.html"
+  },
+  {
+    "revision": "e8e8176697feb9f20fae",
+    "url": "/narrationizer/static/css/main.6a6c2991.chunk.css"
+  },
+  {
+    "revision": "3560fe3d2a6a177f04f3",
+    "url": "/narrationizer/static/js/2.ce80f422.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/narrationizer/static/js/2.ce80f422.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8e8176697feb9f20fae",
+    "url": "/narrationizer/static/js/main.0067f22f.chunk.js"
+  },
+  {
+    "revision": "349d0469e73caf7d6fb7",
+    "url": "/narrationizer/static/js/runtime-main.be86c80b.js"
+  }
+]);
